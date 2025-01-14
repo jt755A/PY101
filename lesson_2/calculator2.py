8a8fc99b73a01f6acf3d215aed229c5e631afc7f
jt755A <jot755A@gmail.com>
2025-01-14 10:38:01 -0800

    progress on calculator

--- lesson_2/calculator2.py
+++ lesson_2/calculator2.py
@@ -1,7 +1,7 @@
 import json
 
-with open('file.json', 'r') as file:
-    data = json.load(file)
+with open('calculator_messages.json', 'r') as file:
+    MESSAGES = json.load(file)
 
 def prompt(message):
     print(f"==> {message}")
@@ -14,34 +14,46 @@ def invalid_number(number_str):
         
     return False
     
-prompt('Welcome to Calculator!')
+# prompt('Welcome to Calculator!')
+prompt(MESSAGES["welcome"])
+
 
 
 keep_going = True
 
 while keep_going:
 
-    prompt("What's the first number?")
+    # prompt("What's the first number?")
+    prompt(MESSAGES["first_num"])
+
     number1 = input()
 
     while invalid_number(number1):
-        prompt("Hmm... that doesn't look like a valid number.")
+        # prompt("Hmm... that doesn't look like a valid number.")
+        prompt(MESSAGES["invalid_num"])
+
         number1 = input()
 
-    prompt("What's the second number?")
+    # prompt("What's the second number?")
+    prompt(MESSAGES["second_num"])
     number2 = input()
 
     while invalid_number(number2):
-        prompt("Hmm... that doesn't look like a valid number.")
+        # prompt("Hmm... that doesn't look like a valid number.")
+        prompt(MESSAGES["invalid_num"])
+
         number2 = input()
         
 
-    prompt("""What operation do you want to perform?
-    1) Add 2) Subtract 3) Multiply 4) Divide""")
+    # prompt("""What operation do you want to perform?
+    # 1) Add 2) Subtract 3) Multiply 4) Divide""")
+    prompt(MESSAGES["operation"])
     operation = input()
 
     while operation not in ["1", "2", "3", "4"]:
-        prompt('You must choose 1, 2, 3, or 4')
+        # prompt('You must choose 1, 2, 3, or 4')
+        prompt(MESSAGES["invalid_operation"])
+
         operation = input()
 
     match operation:
@@ -54,20 +66,26 @@ while keep_going:
         case '4':
             output = int(number1) / int(number2)
 
-    prompt(f'The result is: {output}')
+    # prompt(f'The result is: {output}')
+    prompt(MESSAGES["output"]); prompt(output)
 
-    prompt(f"Do you want to perform another calculation?"
-    "\n Yes/no")
-    answer = input()
+
+    # prompt(f"Do you want to perform another calculation?"
+    # "\nEnter 'Yes or no'")
+    prompt(MESSAGES["another"])
+    answer = input().casefold()
 
     while answer.casefold() not in ['no'.casefold(), 'Yes'.casefold()]:
-        prompt('You must choose Yes or no.')
-        answer = input()
+        # prompt('You must choose Yes or no.')
+        prompt(MESSAGES["invalid_op"])
+
+        answer = input().casefold()
     
     if answer == 'Yes'.casefold():
         continue
     if answer == 'No'.casefold():
-        prompt('End of calculation!')
+        # prompt('End of calculation!')
+        prompt(MESSAGES["end"])
         break
 
 
