a2a915e3f6fffb72b7dd7e76400c0c642132de5f
jt755A <jot755A@gmail.com>
2025-03-09 18:05:13 -0700

    Updated mortgage calcuator after receiving code review

--- lesson_2/mortgage_calc/mortgage_calc.py
+++ lesson_2/mortgage_calc/mortgage_calc.py
@@ -1,56 +1,77 @@
 # Mortage Calculator
 import os
+import math
 
 def prompt(message):
     print(f'--> {message}')
 
-def invalid_number(number_str):  # checks non-numeric + negative values
+
+def invalid_loan(number_str):  # checks non-numeric, 0 and negative values
     try:
         float(number_str)
     except ValueError:
         return True
-    if float(number_str) < 0:
+    if (float(number_str) <= 0
+        or not math.isfinite(float(number_str))):
+
         return True
 
     return False
+
+
+def invalid_rate(number_str):  # checks non-numeric + negative values
+    try:
+        float(number_str)
+    except ValueError:
+        return True
+    if (float(number_str) < 0
+        or not math.isfinite(float(number_str))):
+        return True
+
+    return False
+
 
 def invalid_length(number_str):  # checks non-numeric, 0, and negative values
     try:
-        float(number_str)
+        int(number_str)
     except ValueError:
         return True
-    if float(number_str) <= 0:
+    if (float(number_str) <= 0
+        or not math.isfinite(float(number_str))):
         return True
 
     return False
 
-while True:
 
-    os.system('clear')
-    prompt("Welcome to the Mortgage/Loan Calculator!\n")
-
+def get_loan_amount():
     prompt("What is the loan amount? ($)")
 
     # removing dollar sign, spaces, and commas from input, if any
-    loan_amount = input().strip('$').replace(' ', '').replace(',', '')
+    loan = input().strip('$').replace(' ', '').replace(',', '')
 
-    while invalid_number(loan_amount):
+    while invalid_loan(loan):
         prompt("That looks like an invalid number. Enter a positive number!")
-        loan_amount = input().strip('$').replace(' ', '').replace(',', '')
+        loan = input().strip('$').replace(' ', '').replace(',', '')
 
-    loan_amount = float(loan_amount)
+    loan = float(loan)
+    return loan
 
+
+def get_monthly_interest_rate():
     prompt("What is the Annual interest rate? (Please enter the %)")
     apr = input()
 
-    while invalid_number(apr.strip('%')):
+    while invalid_rate(apr.strip('%')):
         prompt("That looks like an invalid number. Enter a positive number!")
         apr = input()
 
     apr = apr.strip('%') # removing % symbol
     apr = float(apr) / 100 # final figure
-    monthly_interest_rate = apr / 12
+    rate_monthly = apr / 12
+    return rate_monthly
 
+
+def get_time_unit():
     prompt("Is the loan duration specified in:\n1) Years 2) Months?")
     time_unit = input()
 
@@ -60,11 +81,15 @@ while True:
 
     match time_unit:
         case '1':
-            unit = 'years'
+            time_unit = 'years'
+            return time_unit
         case '2':
-            unit = 'months'
+            time_unit = 'months'
+            return time_unit
 
-    prompt(f"How many {unit}?")
+
+def get_loan_duration(time_unit):
+    prompt(f"How many {time_unit}?")
     loan_length = input()
 
     while invalid_length(loan_length):
@@ -72,32 +97,52 @@ while True:
         loan_length = input()
 
     match time_unit:
-        case '1':
-            loan_months = float(loan_length) * 12
-        case '2':
-            loan_months = float(loan_length)
+        case 'years':
+            month_no = float(loan_length) * 12
+            return month_no
+        case 'months':
+            month_no = float(loan_length)
+            return month_no
 
-    if apr == 0:
-        monthly_payment = loan_amount / loan_months
+
+def calculate_monthly_pay(loan, rate, months):
+    if rate == 0:
+        monthly_pay = loan / months
     else:
-        monthly_payment = (loan_amount * (monthly_interest_rate
-                            / (1 - (1 + monthly_interest_rate)
-                               ** (-loan_months)))
-                            )
-    rounded_payment = round(monthly_payment, 2)
+        monthly_pay = (loan * (rate / (1 - (1 + rate) ** (-months))))
+    return monthly_pay
 
-    prompt(f'Your monthly payment is ${rounded_payment}')
 
+def calculate_again():
     prompt("Would you like to make another calculation?")
-    another_answer = input().casefold()
+    answer = input().casefold()
 
     while True:
-        if another_answer.startswith('y') or another_answer.startswith('n'):
-            break
+        if answer in ['yes', 'no', 'y', 'n']:
+            return answer
 
         prompt('Please enter "y" or "n"')
-        another_answer = input().casefold()
+        answer = input().casefold()
 
-    if another_answer[0] == 'n':
-        prompt("Thank you for using the Mortgage/Loan calculator!")
-        break
+def calculator():
+
+    while True:
+
+        os.system('clear')
+        prompt("Welcome to the Mortgage/Loan Calculator!\n")
+
+        loan_amount = get_loan_amount()
+        monthly_interest_rate = get_monthly_interest_rate()
+        unit = get_time_unit()
+        loan_months = get_loan_duration(unit)
+
+        monthly_payment = calculate_monthly_pay(loan_amount, monthly_interest_rate, loan_months)
+        prompt(f'Your monthly payment is ${monthly_payment:.2f}')
+
+        another_answer = calculate_again()
+
+        if another_answer[0] == 'n':
+            prompt("Thank you for using the Mortgage/Loan calculator!")
+            break
+
+calculator()
